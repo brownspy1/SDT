21d50377a9ed7d20a7cda279b0ee3fbed93fc720
brownspy1 <brownspy02@gmail.com>
2025-02-16 23:16:28 +0600

    add

--- Python/Function/local_global.py
+++ Python/Function/local_global.py
@@ -1,4 +1,22 @@
+#Global scop
 blance = 500
 def chk():
     print(blance)
 chk()
+
+# def cal():
+#     blance = blance-5
+#     print(blance) //accebal na
+# cal()
+def gl():
+    global blance
+    blance = blance-5
+    print(blance)
+gl()
+
+#local scop
+def ami():
+    name = "mahadi"
+    print(name)
+ami()
+# print(name) // not accebal
