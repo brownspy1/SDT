a16165c86f10b082a3a0fc80c5cdf15244f0b35d
brownspy1 <brownspy02@gmail.com>
2025-03-15 03:04:09 +0600

    Add views and templates for home, about, and contact pages; configure URL routing

--- Django/FirstApp/FirstApp/urls.py
+++ Django/FirstApp/FirstApp/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
-urlpatterns = [
+urlpatterns = [ 
     path('admin/', admin.site.urls),
+    path('', views.Home, name='home'),
+    path("contact/", views.Contact, name="contact"),
+    path("about/", views.About, name="about")
 ]
