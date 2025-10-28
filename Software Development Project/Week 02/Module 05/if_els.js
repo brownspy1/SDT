c4000aba3fa6963d9b6639222ae777df2cbbb6c0
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2025-10-28 22:44:17 +0600

    add

---
+++ Software Development Project/Week 02/Module 05/if_els.js
@@ -0,0 +1,7 @@
+var ami = false;
+
+if (ami) {
+    console.log("ami asi");
+}else{
+    console.log("Ami nai");
+}
---
+++ Software Development Project/Week 02/Module 05/object.js
@@ -0,0 +1,12 @@
+var my_details = {
+    name : "M.Mahadi",
+    age : "20",
+    dep:"CST" ,
+    tag:{
+        normal:"ABC",
+        Header:"AbC",
+        Hardest:"SUYGQDBVHAsaijfdbjdnasjbinsijbBSIJAJB*HSBAIJDadbainbIJABIBSIHABsijxaBhi"
+    }
+};
+
+console.log(my_details.tag.normal);
