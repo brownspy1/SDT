7d4ea6eccae89af12e7520ad294741bb89de02ef
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2025-10-28 23:38:51 +0600

    add

---
+++ Software Development Project/Week 02/Module 05/array.js
@@ -0,0 +1,10 @@
+var listofgandu = ["Abujar","Rakib","arafat"];
+
+listofgandu.push("Rahim");
+console.log(listofgandu);
+listofgandu.pop();
+console.log(listofgandu);
+listofgandu.unshift("Karim");
+console.log(listofgandu);
+listofgandu.shift();
+console.log(listofgandu);
---
+++ Software Development Project/Week 02/Module 05/forLoop.js
@@ -0,0 +1,11 @@
+var gandus = ["rahim","karim","javed","david"];
+
+for(var i = 0; i < gandus.length; i++){
+    var gandu = gandus[i];
+
+    if (gandu == "david") {
+        console.log("Yes Sir");
+    }else{
+        console.log("No sir");
+    }
+}
