9368dae0471213d9d6d54a77e335c80859bc8d1f
brownspy1 <brownspy02@gmail.com>
2026-05-14 00:54:45 +0600

    delete

--- Django/Task_manager/taskManegment/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from tasks.views import home
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',home),
-    path('tasks/',include('tasks.urls'))
-]
--- Django/Task_manager/tasks/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from tasks.views import add_task
-urlpatterns = [
-    path('add_task',add_task)
-]
- 
